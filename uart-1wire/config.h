21dcd4eefb34e5d001c29845f7d277b9a20742e2
Vadim Velikodniy <vadim@velikodniy.name>
2015-01-11 01:01:53 +0200

    Fix comment

--- uart-1wire/config.h
+++ uart-1wire/config.h
@@ -6,7 +6,7 @@
 // UART settings
 #define BAUD					9600		// UART Baud Rate, BPS
 #define UART_USE_2X				0			// Use 2x UART speed
-#define UART_STDIN_ECHO			1			// Disable echo for stdin (eg. in scanf)
+#define UART_STDIN_ECHO			1			// Enable echo for stdin (eg. in scanf)
 #define UART_FIX_LINE_ENDING	1			// Replace: \n -> \r\n
 
 // 1-wire settings
