de39eed5b2bfe27bd154f0f43e78c5c7661dd67b
Vadim Velikodniy <vadim@velikodniy.name>
2015-01-10 23:55:32 +0200

    Sections in config.h

--- uart-1wire/config.h
+++ uart-1wire/config.h
@@ -2,9 +2,13 @@
 #define CONFIG_H_
 
 #define F_CPU					8000000L	// CPU Frequency, Hz
+
+// UART settings
 #define BAUD					9600		// UART Baud Rate, BPS
-#define UART_USE_2X					0			// Use 2x UART speed
+#define UART_USE_2X				0			// Use 2x UART speed
 #define UART_STDIN_ECHO			1			// Disable echo for stdin (eg. in scanf)
 #define UART_FIX_LINE_ENDING	1			// Replace: \n -> \r\n
+
+
 
 #endif /* CONFIG_H_ */
