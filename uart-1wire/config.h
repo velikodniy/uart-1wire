cf6a15f0d8f1b6102899867fe83dcc839fd7158a
Vadim Velikodniy <vadim@velikodniy.name>
2015-01-10 23:43:41 +0200

    USE_2X option in config.h

--- uart-1wire/config.h
+++ uart-1wire/config.h
@@ -1,9 +1,9 @@
 ﻿#ifndef CONFIG_H_
 #define CONFIG_H_
 
-
 #define F_CPU			8000000L	// CPU Frequency, Hz
 #define BAUD			9600		// UART Baud Rate, BPS
-#define UART_STDIN_ECHO				// Disable echo for stdin (eg. in scanf)
+#define USE_2X			0			// Use 2x UART speed
+#define UART_STDIN_ECHO	1			// Disable echo for stdin (eg. in scanf)
 
 #endif /* CONFIG_H_ */
