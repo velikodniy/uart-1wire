e92e04867a2f4321a2a364733bbab4f375fa9b21
Vadim Velikodniy <vadim@velikodniy.name>
2015-01-13 22:58:09 +0200

    New main program

--- uart-1wire/uart-1wire.c
+++ uart-1wire/uart-1wire.c
@@ -1,18 +1,27 @@
-﻿#include <stdio.h>
+﻿#include "config.h"
+#include <stdio.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include "uart.h"
+#include "onewire.h"
+#include "ds18x20.h"
 
-#include <math.h>
+#define MAXDEVICES 8 // Maximum number of devices on line
+
+uint8_t     num_of_devices;
+OW_ROM_t    devices[MAXDEVICES];
 
 void interactive(void);
+void print_address(uint8_t* addr);
+void print_devices_list(void);
 
 int main(void)
 {
     char cmd;
     
     UART_init();
+    num_of_devices = OW_SearchDevices(devices, MAXDEVICES);
     
     // Main loop
     while (1) {
@@ -35,100 +44,59 @@ void interactive(void) {
         switch(cmd) {
         case 'q':
             return;
+        case 'd':
+            print_devices_list();
+            break;
         default:
             puts_P(PSTR("Unknown command"));
         }
     }
 }
 
-// Device processing
-/*
-#define MAXDEVICES                8            // Maximum number of devices on line
-
-void print_address(unsigned char* address) {
-    printf("%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X", address[0],address[1],address[2],address[3],address[4],address[5],address[6],address[7]);
+void print_address(uint8_t* addr) {
+    for (uint8_t i = 0; i < 8; i++)
+        printf_P(PSTR("%.2X "), addr[i]);
 }
 
-unsigned char    nDevices;    // количество сенсоров
-unsigned char    owDevicesIDs[MAXDEVICES][8];    // Их ID
-
-unsigned char search_ow_devices(void) // поиск всех устройств на шине
+void print_devices_list(void)
 {
-    unsigned char    i;
-    unsigned char    id[OW_ROMCODE_SIZE];
-    unsigned char    diff, sensors_count;
+    printf_P(PSTR("Found %d device(s)\n"), num_of_devices);
 
-    sensors_count = 0;
-
-    for( diff = OW_SEARCH_FIRST; diff != OW_LAST_DEVICE && sensors_count < MAXDEVICES ; )
-    {
-        OW_FindROM( &diff, &id[0] );
-
-        if( diff == OW_PRESENCE_ERR ) break;
-
-        if( diff == OW_DATA_ERR )    break;
-
-        for (i=0;i<OW_ROMCODE_SIZE;i++)
-        owDevicesIDs[sensors_count][i] = id[i];
+    for (uint8_t i = 0; i < num_of_devices; i++) {
+        print_address(devices[i]);
+        switch (devices[i][0]) {
+        case OW_DS18B20_FAMILY_CODE:
+            printf_P(PSTR("Thermometer DS18B20"));
+            DS18x20_SetResolution(devices[i], 12);
+            DS18x20_StartMeasure(devices[i]);
+            DS18x20_WaitForMeasure(12);
+            uint8_t data[2];
+            DS18x20_ReadData(devices[i], data);
+            int8_t t[2];
+            DS18x20_DataConvert(data, t);
+            printf_P(PSTR(": %d,%02d C"), t[0], t[1]);
+            break;
         
-        sensors_count++;
-    }
-    return sensors_count;
+        case OW_DS18S20_FAMILY_CODE:
+            printf_P(PSTR("Thermometer DS18S20"));
+            break;
 
-}
-unsigned char    themperature[3]; // в этот массив будет записана температура
-int main1(void)
-{
-    stdout = &usart_str; // указываем, куда будет выводить printf
-
-    DDRB = 0b00000010; PORTB = 0b00000010;
-    DDRC = 0b00000000; PORTC = 0b00000000;
-    DDRD = 0b00000010; PORTD = 0b00000000;
-
-    USART_init(); // включаем uart
-
-    timerDelayInit();
-
-    nDevices = search_ow_devices(); // ищем все устройства
-
-    printf("---------- Found %d devices ----------", nDevices);
-
-
-    for (unsigned char i=0; i<nDevices; i++) // теперь сотируем устройства и запрашиваем данные
-    {
-        // узнать устройство можно по его груповому коду, который расположен в первом байте адресса
-        switch (owDevicesIDs[i][0])
-        {
-            case OW_DS18B20_FAMILY_CODE: { // если найден термодатчик DS18B20
-                printf("\r"); print_address(owDevicesIDs[i]); // печатаем знак переноса строки, затем - адрес
-                printf(" - Thermometer DS18B20"); // печатаем тип устройства
-                DS18x20_StartMeasure(owDevicesIDs[i]); // запускаем измерение
-                timerDelayMs(800); // ждем минимум 750 мс, пока конвентируется температура
-                unsigned char    data[2]; // переменная для хранения старшего и младшего байта данных
-                DS18x20_ReadData(owDevicesIDs[i], data); // считываем данные
-                
-                float t = DS18x20_ConvertToThemperatureFl(data); // преобразовываем температуру в человекопонятный вид
-                printf(": %3.2f C", t);
-            } break;
-            case OW_DS18S20_FAMILY_CODE: { // если найден термодатчик DS18B20
-                printf("\r"); print_address(owDevicesIDs[i]); // печатаем знак переноса строки, затем - адрес
-                printf(" - Thermometer DS18S20"); // печатаем тип устройства
-            } break;
-
-            case OW_DS1990_FAMILY_CODE: { // если найден электронный ключ DS1990
-                printf("\r"); print_address(owDevicesIDs[i]); // печатаем знак переноса строки, затем - адрес
-                printf(" - Serial button DS1990"); // печатаем тип устройства
-            } break;
-            case OW_DS2430_FAMILY_CODE: { // если найдена EEPROM
-                printf("\r"); print_address(owDevicesIDs[i]); // печатаем знак переноса строки, затем - адрес
-                printf(" - EEPROM DS2430"); // печатаем тип устройства
-            } break;
-            case OW_DS2413_FAMILY_CODE: { // если найден ключ
-                printf("\r"); print_address(owDevicesIDs[i]); // печатаем знак переноса строки, затем - адрес
-                printf(" - Switch 2413"); // печатаем тип устройства
-            } break;
+        case OW_DS1990_FAMILY_CODE:
+            printf_P(PSTR("Serial button DS1990"));
+            break;
+        
+        case OW_DS2430_FAMILY_CODE:
+            printf_P(PSTR("EEPROM DS2430"));
+            break;
+        
+        case OW_DS2413_FAMILY_CODE:
+            printf_P(PSTR("Switch 2413"));
+            break;
+            
+        default:
+            printf_P(PSTR("Unknown device"));
         }
-
+        
+        printf_P(PSTR("\n"));
     }
-*/
-
+}
