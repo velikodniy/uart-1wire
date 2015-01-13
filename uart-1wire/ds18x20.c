20efddc9068c21d7853f28dc8ab5a801d3a02ec0
Vadim Velikodniy <vadim@velikodniy.name>
2015-01-13 19:15:39 +0200

    Unneeded code was removed

--- uart-1wire/ds18x20.c
+++ uart-1wire/ds18x20.c
@@ -15,41 +15,6 @@ uint8_t DS18x20_StartMeasure(uint8_t* rom)
 }
 
 #ifdef DS18X20_CHECK_CRC
-
-/*
-#define CRC8INIT    0x00
-#define CRC8POLY    0x18              //0X18 = X^8+X^5+X^4+X^0
-
-uint8_t crc8(uint8_t *data_in, unsigned int number_of_bytes_to_read )
-{
-    uint8_t    crc;
-    unsigned int    loop_count;
-    uint8_t    bit_counter;
-    uint8_t    data;
-    uint8_t    feedback_bit;
-
-    crc = CRC8INIT;
-    
-    for (loop_count = 0; loop_count != number_of_bytes_to_read; loop_count++)
-    { 
-        data = data_in[loop_count];
-
-        bit_counter = 8;
-        do { 
-            feedback_bit = (crc ^ data) & 0x01;
-            if (feedback_bit==0x01) crc = crc ^ CRC8POLY;
-
-            crc = (crc >> 1) & 0x7F;
-            if (feedback_bit==0x01) crc = crc | 0x80;
-
-            data = data >> 1;
-            bit_counter--;
-        }
-        while (bit_counter > 0);
-    }
-    return crc;
-} */
-
 // Optimized Dallas (now Maxim) iButton 8-bit CRC calculation.
 uint8_t crc8 (uint8_t *data, uint8_t count) {
     uint8_t crc = 0x00; // Initial value
@@ -57,7 +22,6 @@ uint8_t crc8 (uint8_t *data, uint8_t count) {
         crc = _crc_ibutton_update(crc, data[i]);
     return crc;
 }
-
 #endif 
 
 uint8_t DS18x20_ReadData(uint8_t *rom, uint8_t *buffer)
